e3c8078ae4b54ea2937f547dc98078e5c3d39d46
lee-code712 <yurilee07@naver.com>
2023-01-04 14:09:55 +0900

    swagger host 설정 추가

--- src/main/java/com/digital/v3/config/SwaggerConfig.java
+++ src/main/java/com/digital/v3/config/SwaggerConfig.java
@@ -28,6 +28,7 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.consumes(getConsumeContentTypes()).produces(getProduceContentTypes())
+				.host("commerce.yr.inventory:8080")
 				.apiInfo(getApiInfo()).select()
 				.apis(RequestHandlerSelectors.any()).paths(PathSelectors.ant("/rest/**")) // 그 중 /rest/** 인 URL들만 필터링
 				.build()
